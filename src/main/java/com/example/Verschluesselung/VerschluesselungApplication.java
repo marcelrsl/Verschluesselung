e74e73f3dd239a698e3f60695f8fab499ed13b11
marcelrsl <111505325+marcelrsl@users.noreply.github.com>
2023-04-13 12:34:50 +0200

    Add files via upload

---
+++ src/main/java/com/example/Verschluesselung/VerschluesselungApplication.java
@@ -0,0 +1,13 @@
+package com.example.Verschluesselung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerschluesselungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerschluesselungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Verschluesselung/VerschluesselungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Verschluesselung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerschluesselungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
